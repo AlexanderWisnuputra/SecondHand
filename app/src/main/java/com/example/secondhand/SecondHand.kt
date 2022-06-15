8b61cc672c7ef245091bd1adf35591b3657be5c8
nchlfs1 <nicholasf249@gmail.com>
2022-06-15 21:25:00 +0700

    KOIN

---
+++ app/src/main/java/com/example/secondhand/SecondHand.kt
@@ -0,0 +1,18 @@
+package com.example.secondhand
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+
+class SecondHand: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@SecondHand)
+        }
+    }
+
+}
+
