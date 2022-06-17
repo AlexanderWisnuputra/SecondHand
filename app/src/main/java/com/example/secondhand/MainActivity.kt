8566edeffd21cb6a382ffa73dc89aeb4b9650065
AlexanderWisnuputra <ksmwdsz7@gmail.com>
2022-06-17 19:03:51 +0700

    Fix Login and Register Screen

--- app/src/main/java/com/example/secondhand/MainActivity.kt
+++ app/src/main/java/com/example/secondhand/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.secondhand
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
@@ -18,10 +19,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         binding.bottomNavigationView.setupWithNavController(navController)
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.login -> binding.bottomNavigationView.visibility = View.INVISIBLE
+                R.id.register ->binding.bottomNavigationView.visibility = View.INVISIBLE
+                else -> binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
-}
+    }
