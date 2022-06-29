c3db4610155721d92619f674b50df8534ee5fed1
nchlfs1 <nicholasf249@gmail.com>
2022-06-29 20:38:14 +0700

    update

--- app/src/main/java/com/example/secondhand/fragments/ChangeAcc.kt
+++ app/src/main/java/com/example/secondhand/fragments/ChangeAcc.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -15,6 +18,7 @@ import com.example.secondhand.R
 import com.example.secondhand.dao.UserViewModel
 import com.example.secondhand.databinding.FragmentChangeAccBinding
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -37,20 +41,21 @@ class ChangeAcc : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        dataStore = requireContext().createDataStore(name = "user")
         binding.apply {
             btnEdit.setOnClickListener {
-                if (binding.passReg.text.toString() == binding.confPasstext.text.toString())
-                { saveData() }
-                else if (binding.passReg.length() <= 6)
-                {
-                    Toast.makeText(requireContext(),"Password minimal 6 huruf", Toast.LENGTH_LONG).show()
-                }
-                else{
-                    Toast.makeText(requireContext(),"Data tidak sesuai!", Toast.LENGTH_LONG).show()
-                }
+                saveData()
             }
-            btnBatal.setOnClickListener { findNavController().navigate(R.id.action_changeAcc_to_profileDetail) }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        lifecycleScope.launch{
+            val loginCheck = read("login")
+            if (loginCheck == "") {
+                login()
+            }
         }
     }
 
@@ -58,10 +63,10 @@ class ChangeAcc : Fragment() {
         lifecycleScope.launch(Dispatchers.IO) {
             viewModel.userProfile(
                 args.dataAcc.id!!,
-                args.dataAcc.name,
+                binding.namaUbah.text.toString(),
                 args.dataAcc.email,
-                binding.passReg.text.toString(),
-                binding.kotaProvinsi.text.toString(),
+                args.dataAcc.password,
+                binding.kota.text.toString(),
                 args.dataAcc.noHP,
                 args.dataAcc.picture
             )
@@ -70,5 +75,24 @@ class ChangeAcc : Fragment() {
                 findNavController().navigate(R.id.action_changeAcc_to_profileDetail)
             }
         }
+    }
+
+    private fun login() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setMessage("Anda belum login, apakah anda ingin login ?")
+            .setPositiveButton("Ya") {_, _->
+                findNavController().navigate(R.id.action_changeAcc_to_login)
+            }
+            .setNegativeButton("Tidak") {_, _->
+                findNavController().navigate(R.id.action_changeAcc_to_profileDetail)
+                Toast.makeText(requireContext(), "Untuk menambahkan info, anda harus login terlebih dahulu", Toast.LENGTH_LONG).show()
+            }
+            .show()
+    }
+
+    private suspend fun read(key: String): String? {
+        val dataStoreKey = preferencesKey<String>(key)
+        val preferences = dataStore.data.first()
+        return preferences[dataStoreKey]
     }
 }
